98781e158925ca5f9c67bc0b4969d8923c1ff7b8
acslook <acslook@hotmail.com>
2024-07-06 18:17:26 -0300

    first commit.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # proxy-reverse-nginx-ssl
+* Exemplo de proxy reverso com Nginx usando SSL Self Signed certificate.
+
+## Gerar e auto-assinar um certificado SSL
+1. Instalar openssl no linux 
+`sudo apt-get install openssl`
+
+2. Depois de instalado, você pode gerar o certificado com o seguinte comando: openssl 
+`sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt`
+
+3. Criar dhparam 
+`sudo openssl dhparam -out /etc/nginx/dhparam.pem 4096`
+
+4. Configurar parametros de acordo com o arquivo self-signed.conf
+
+5. Executar `docker compose up`
---
+++ WebApiTesteNginx/GlobalExceptionFilter.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+public class GlobalExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        var statusCode = context.Exception switch
+        {
+            NotFoundException => StatusCodes.Status404NotFound,
+
+            ValidationException => StatusCodes.Status400BadRequest,
+
+            UnauthorizedAccessException => StatusCodes.Status401Unauthorized,
+
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        context.Result = new ObjectResult(new
+        {
+            error = context.Exception.Message,
+            stackTrace = context.Exception.StackTrace
+        })
+        {
+            StatusCode = statusCode
+        };
+    }
+}
